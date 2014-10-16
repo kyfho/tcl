a17d1601778d7d1c97070be0e7b19b4bf581e63a
kyfho <kyfho@users.noreply.github.com>
2014-10-15 23:07:22 -0700

    Create avg.tcl

---
+++ avg.tcl
@@ -0,0 +1,12 @@
+#!/usr/local/bin/tclsh8.6
+proc avg args {
+set n 0
+set t 0
+foreach arg $args {
+set n [expr {$n + 1}]
+set t [expr {$t + $arg}]
+}
+puts "Avg is [expr {$t / $n}]"
+}
+
+avg 4 5.5 9 12
